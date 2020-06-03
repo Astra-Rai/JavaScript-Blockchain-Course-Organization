034783fd11f221f67d9051a0dce22df0d5a5c189
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-06-03 08:29:44 -0400

    Part 2_JS_Code_Import crypto-js/SHA256 Library

---
+++ Part_2_Install and Import and Crypto JS Library to Use SHA256.js
@@ -0,0 +1,13 @@
+const SHA256 = require ('crypto-js/sha256');//library crypto-js/sha256 is used in calculateHash()method to calculate the has function
+
+class Block {//block class constains constuctor for block parameters and calculateHash() method
+
+    constructor(index, timestamp, data, previousHash = ''){//constructor:received properties of each block, which are in ()
+       
+        this.index = index; //index parameter: optional, tells uslocation of block on chain, remember chain is an array
+        this.timestamp = timestamp; // timestamp parameter: provides date and time block created 
+        this.data = data;//data parameter: could include: fromAddress, toAddress, amountSent
+        this.previousHash = previousHash;//previousHash parameter: hash of prior block
+        this.hash = this.calculateHash();//hash property: has value calculated by calculateHash() method
+    }
+}
